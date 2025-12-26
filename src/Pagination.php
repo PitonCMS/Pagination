44b292b1bcb69b48c6f2c969797a0e13d30b208d
Wolfgang Moritz <wolfmoritz@gmail.com>
2025-12-26 13:50:57 +0000

    Updating for PHP8

--- src/Pagination.php
+++ src/Pagination.php
@@ -4,7 +4,7 @@
  * PitonCMS (https://github.com/PitonCMS)
  *
  * @link      https://github.com/PitonCMS/Piton
- * @copyright Copyright 2015 Wolfgang Moritz
+ * @copyright Copyright 2015-2026 Wolfgang Moritz
  * @license   https://github.com/PitonCMS/Piton/blob/master/LICENSE (MIT License)
  */
 
@@ -29,7 +29,7 @@ class Pagination
      * @param  void
      * @return void
      */
-    public function __invoke()
+    public function __invoke(): void
     {
         $this->buildPageLinks();
         $this->render();
--- src/templates/pageLinks.php
+++ src/templates/pageLinks.php
@@ -4,7 +4,7 @@
  * PitonCMS (https://github.com/PitonCMS)
  *
  * @link      https://github.com/PitonCMS/Piton
- * @copyright Copyright 2015 Wolfgang Moritz
+ * @copyright Copyright 2015-2026 Wolfgang Moritz
  * @license   https://github.com/PitonCMS/Piton/blob/master/LICENSE (MIT License)
  */
 
